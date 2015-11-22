73dc8b173dafc1d326c52f9e69bd267bb85e5073
saddam213 <sa_ddam213@live.com>
2015-11-22 08:24:28 +0000

    Add Email settings to config

--- TradeSatoshi.Core/Services/EmailService.cs
+++ TradeSatoshi.Core/Services/EmailService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using TradeSatoshi.Common;
@@ -11,6 +13,11 @@ namespace TradeSatoshi.Core.Services
 {
 	public class EmailService : IEmailService
 	{
+		private readonly string EmailUser = ConfigurationManager.AppSettings["SMTP_User"];
+		private readonly string EmailPassword = ConfigurationManager.AppSettings["SMTP_Password"];
+		private readonly string EmailServer = ConfigurationManager.AppSettings["SMTP_Server"];
+		private readonly string EmailPort = ConfigurationManager.AppSettings["SMTP_Port"];
+
 		public bool Send(EmailTemplate template, params object[] formatParameters)
 		{
 			return true;
