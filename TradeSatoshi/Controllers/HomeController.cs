77e0869a16e087ec1bb8bb3d1de610c26f81e1cc
TripleHeXXX <hex@triplehexxx.net>
2015-11-22 20:55:01 +1300

    Vote list view and model

--- TradeSatoshi/Controllers/HomeController.cs
+++ TradeSatoshi/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using System.Web.Mvc;
+using System.Threading.Tasks;
+using TradeSatoshi.Models;
 
 namespace TradeSatoshi.Controllers
 {
@@ -18,5 +20,46 @@ namespace TradeSatoshi.Controllers
         {
             return View();
         }
+
+        #region Coin Voting
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<ActionResult> Voting()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Authorize]
+        public ActionResult SubmitVote()
+        {
+            return RedirectToAction("Voting");
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> SubmitVote(int voteItemId)
+        {
+                return RedirectToAction("Voting");
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult CreateVoteItem()
+        {
+            return View(new CreateItemModel());
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> SubmitVoteItem(CreateItemModel model)
+        {
+            return RedirectToAction("Voting");
+        }
+
+        #endregion
 	}
 }
