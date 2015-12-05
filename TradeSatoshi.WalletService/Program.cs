b012826c1e5012a10fafdf4a0bb35c01b5445315
saddam213 <sa_ddam213@live.com>
2015-12-05 10:48:09 +0000

    Wallet service added

---
+++ TradeSatoshi.WalletService/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeSatoshi.WalletService
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		static void Main()
+		{
+#if DEBUG
+			using (var processor = new WalletService())
+			{
+				processor.StartService();
+				Console.WriteLine("Press Enter to terminate ...");
+				Console.ReadLine();
+				processor.StopService();
+			}
+#else
+
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+            { 
+                new WalletService() 
+            };
+            ServiceBase.Run(ServicesToRun);
+#endif
+		}
+	}
+}
---
+++ TradeSatoshi.WalletService/WalletService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeSatoshi.WalletService
+{
+	public partial class WalletService : ServiceBase
+	{
+		public WalletService()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnStart(string[] args)
+		{
+		}
+
+		protected override void OnStop()
+		{
+		}
+
+		internal void StartService()
+		{
+			OnStart(null);
+		}
+
+		internal void StopService()
+		{
+			OnStop();
+		}
+	}
+}
