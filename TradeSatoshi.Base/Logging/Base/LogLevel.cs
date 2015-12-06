3836add454f65a5bb5234ace45600cda705c9f7f
saddam213 <sa_ddam213@live.com>
2015-12-06 06:30:09 +0000

    Logger added

---
+++ TradeSatoshi.Base/Logging/Base/LogLevel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeSatoshi.Base.Logging
+{
+	public enum LogLevel : int
+	{
+		Verbose = 0,
+		/// <summary>
+		/// Debug message
+		/// </summary>
+		Debug = 1,
+		/// <summary>
+		/// Info message
+		/// </summary>
+		Info = 2,
+		/// <summary>
+		/// Warn message
+		/// </summary>
+		Warn = 3,
+		/// <summary>
+		/// Error message
+		/// </summary>
+		Error = 4,
+		/// <summary>
+		/// No Logging
+		/// </summary>
+		None = 5,
+	}
+}
---
+++ TradeSatoshi.Base/Logging/ConsoleLogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeSatoshi.Base.Logging
+{
+	public class ConsoleLogger : Logger
+	{
+		public ConsoleLogger(LogLevel level)
+			: base(level)
+		{
+		}
+
+		protected override void LogQueuedMessage(string message)
+		{
+			Console.WriteLine(message);
+		}
+	}
+}
